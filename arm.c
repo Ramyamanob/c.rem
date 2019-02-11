f34d1a59449308ec7ac5468382ba702692fe44fe
Ramyamanob <47448974+Ramyamanob@users.noreply.github.com>
2019-02-11 14:36:17 +0530

    Update and rename README.md to arm.c

---
+++ arm.c
@@ -0,0 +1,23 @@
+# c.rem
+#include<stdio.h>
+int main()
+{
+    int n,r,sum=0,t;
+    scanf("%d",&n);
+    t=n;
+    while(n!=0)
+    {
+        r=n%10;
+        sum=sum+r*r*r;
+        n=n/10;
+    }
+    if(sum==t)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
